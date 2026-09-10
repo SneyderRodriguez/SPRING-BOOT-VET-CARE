e5a3cb7c34a923bbfbe14b7c3d7c2c6c39078893
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-09-10 09:48:03 -0500

    Uso de Spring Security y JWT

---
+++ src/main/java/com/generation/vetcare/model/RolUsuario.java
@@ -0,0 +1,7 @@
+package com.generation.vetcare.model;
+
+public enum RolUsuario {
+    ADMIN,
+    VETERINARIO,
+    RECEPCIONISTA
+}
---
+++ src/main/java/com/generation/vetcare/model/Usuario.java
@@ -0,0 +1,34 @@
+package com.generation.vetcare.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private RolUsuario rol;
+}
